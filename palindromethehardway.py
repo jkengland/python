d401c9999bf4d276c31fddc80e95abcca77c3bca
jason_k_england <jason_k_england@dell.com>
2022-05-06 13:43:22 -0500

    updated with notes

--- palindromethehardway.py
+++ palindromethehardway.py
@@ -1,4 +1,10 @@
-# first take the input and count the charcters in the input. 
-# second take the input and reverse it. This must be done without modules 
-# third compare the input with the reversed result 
+userword = input("Please type the word you would like to test.: ")
 
+myword = 'tacocat'
+# 
+
+len tacocat
+# compare character 0 with last character -1. Character 0 = character at string lenght -1
+# compare character 1 with last character -2. Character 1 = character at string lenght -2
+# compare character 2 with last character -3. Character 2 = character at string lenght -3
+# compare character 3 with last character -4. Character 3 = character at string lenght -4
